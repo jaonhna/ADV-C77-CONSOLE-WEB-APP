42a03a1ebd960b88e7d3909d71f28963ef786cdb
jaonhna <69300428+jaonhna@users.noreply.github.com>
2021-05-03 18:55:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var num5 = 5;
+var num6 = 3;
+
+// add two numbers
+var sum = num5 + num6;
+
+// display the sum
+console.log('The sum of ' + num5 + ' and ' + num6 + ' is: ' + sum);
+
+var num1 = 5;
+var num2 = 3;
+
+// add two numbers
+var difference = num1 - num2;
+
+// display the sum
+console.log('The difference of ' + num1 + ' and ' + num2 + ' is: ' + difference);
+
+var num4 = 5;
+var num3 = 3;
+
+// add two numbers
+var product = num4 * num3;
+
+// display the sum
+console.log('The product of ' + num4 + ' and ' + num3 + ' is: ' + product);
+
+var num7 = 5;
+var num8 = 3;
+
+// add two numbers
+var quotient = num7 / num8;
+
+// display the sum
+console.log('The quotient of ' + num7 + ' and ' + num8 + ' is: ' + quotient);
+
+
+
